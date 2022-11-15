ef35e976aacc2d577890d317b698056dd84d462f
Maciej <158809@student.uwm.edu.pl>
2022-11-15 23:04:50 +0100

    Utworzenie i Edycja bazy danych

--- ML_app/settings.py
+++ ML_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'ML_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'JangoAP',
+        'PASSWORD': 'MaciejL2169',
+        'NAME': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -106,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT+1'
 
 USE_I18N = True
 
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Gun(models.Model):
+    Gun_name = models.CharField(max_length=200)
+    is_legal = models.BooleanField('Is legal?')
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    manufacturer = models.ForeignKey(Gun, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.question_text
