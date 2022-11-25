32c05d74961b56c02ada7a76907afbc0e9c36754
Maciej <158809@student.uwm.edu.pl>
2022-11-25 15:34:17 +0100

    lab_3

--- polls/models.py
+++ polls/models.py
@@ -15,3 +15,45 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.question_text
+
+class Osoba(models.Model):
+    class Meta:
+        ordering = ["nazwisko"]
+        verbose_name_plural = "Osoby"
+
+    class Dates(models.IntegerChoices):
+        JANUARY = 1
+        FEBRUARY = 2
+        MARCH = 3
+        APRIL = 4
+        MAY = 5
+        JUNE = 6
+        JULY = 7
+        AUGUST = 8
+        SEPTEMBER = 9
+        OCTOBER = 10
+        NOVEMBER = 11
+        DECEMBER = 12
+
+    imie = models.CharField(max_length=64, blank=False)
+    nazwisko = models.CharField(max_length=64, blank=False)
+    miesiac_urodzenia = models.IntegerField(choices=Dates.choices, default=Dates.JANUARY)
+    data_dodania = models.DateTimeField(auto_now_add=True)
+    druzyna = models.ForeignKey(
+        'Druzyna',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.imie + ' ' + self.nazwisko
+
+class Druzyna(models.Model):
+    class Meta:
+        verbose_name_plural = "Druzyny"
+    nazwa = models.TextField(max_length=64, blank=False)
+    kraj = models.TextField(max_length=2, blank=False)
+
+    def __str__(self):
+        return self.nazwa + ' (' + self.kraj + ')'
