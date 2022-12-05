93b2a2ca7969902aa80c704b2bce461e0df042b3
Maciej <158809@student.uwm.edu.pl>
2022-12-05 16:25:50 +0100

    lab_7

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('persons/<int:pk>/', views.person_detail, name='person_detail'),
     path('persons/update/<int:pk>/', views.person_update_delete, name='person_update_delete'),
     path('persons/delete/<int:pk>/', views.person_update_delete, name='person_update_delete'),
+    path('persons/permcheck/<int:pk>/', views.person_view, name='person_view'),
     path('', views.index, name='index'),
     path('teams/', views.team_list),
     path('teams/<int:pk>/', views.team_detail)
