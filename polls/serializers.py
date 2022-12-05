35ec324f425b1fff05a9f2aabad3a0b7f3ff79c4
Maciej <158809@student.uwm.edu.pl>
2022-12-05 15:12:11 +0100

    feature_lab_5

---
+++ polls/serializers.py
@@ -0,0 +1,72 @@
+from rest_framework import serializers
+from django.utils import timezone
+from .models import Gun, Choice, Osoba, Druzyna
+
+class GunModelSerializer(serializers.Serializer):
+    model = Gun
+    fields = ['id', 'Gun_name', 'pub_date']
+    read_only_fields = ['id', 'pub_date']
+
+class DruzynaModelSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    nazwa = serializers.CharField(max_length=64)
+    kraj = serializers.CharField(max_length=2)
+
+    def create(self, validated_data):
+        return Druzyna.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.nazwa = validated_data.get('nazwa', instance.nazwa)
+        instance.kraj = validated_data.get('kraj', instance.kraj)
+        instance.save()
+        return instance
+
+class OsobaModelSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    imie = serializers.CharField(max_length=64)
+    nazwisko = serializers.CharField(max_length=64)
+    miesiac_urodzenia = serializers.ChoiceField(choices=Osoba.Dates.choices, default=Osoba.Dates.JANUARY)
+    druzyna = serializers.PrimaryKeyRelatedField(queryset=Druzyna.objects.all(), allow_null=True)
+    data_dodania = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Osoba.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.imie = validated_data.get('imie', instance.imie)
+        instance.nazwisko = validated_data.get('nazwisko', instance.nazwisko)
+        instance.miesiac_urodzenia = validated_data.get('miesiac_urodzenia', instance.miesiac_urodzenia)
+        instance.druzyna = validated_data.get('druzyna', instance.druzyna)
+        instance.save()
+        return instance
+
+    def validate_imie(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("W polu moga znalezc sie tylko litery")
+        return value
+
+    def validate_nazwisko(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("W polu moga znalezc sie tylko litery")
+        return value
+	
+    def validate_data_dodania(self, value):
+        if value > timezone.now():
+            raise serializers.ValidationError("Data urodzenia nie moze byc z przyszlosci")
+        return value
+
+class GunModelSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.PrimaryKeyRelatedField(queryset=Gun.objects.all(), allow_null=True)
+    choice_text = serializers.CharField(required=True)
+    votes = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Gun.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get('manufacturer', instance.manufacturer)
+        instance.choice_text = validated_data.get('choice_text', instance.choice_text)
+        instance.votes = validated_data.get('votes', instance.votes)
+        instance.save()
+        return instance
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,86 @@
+from django.shortcuts import render
+
+from rest_framework import status
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+from .models import Osoba, Druzyna
+from .serializers import OsobaModelSerializer, DruzynaModelSerializer
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.http import HttpResponse
 
 
+# Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+@api_view(['GET'])
+def person_list(request):
+    if request.method == 'GET':
+        persons = Osoba.objects.all()
+        serializer = OsobaModelSerializer(persons, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+def person_detail(request, pk):
+    try:
+        person = Osoba.objects.get(pk=pk)
+    except Osoba.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        person = Osoba.objects.get(pk=pk)
+        serializer = OsobaModelSerializer(person)
+        return Response(serializer.data)
+
+@api_view(['PUT', 'DELETE'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def person_update_delete(request, pk):
+    try:
+        person = Osoba.objects.get(pk=pk)
+    except Osoba.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = OsobaModelSerializer(person, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        person.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET'])
+def team_list(request):
+
+    if request.method == 'GET':
+        teams = Druzyna.objects.all()
+        serializer = DruzynaModelSerializer(teams, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def team_detail(request, pk):
+    try:
+        team = Druzyna.objects.get(pk=pk)
+    except Druzyna.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        team = Druzyna.objects.get(pk=pk)
+        serializer = DruzynaModelSerializer(team)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = DruzynaModelSerializer(team, data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        team.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
