3a3460560e2cffb30952b220d1699728a65f8f28
Maciej <158809@student.uwm.edu.pl>
2022-12-05 15:48:44 +0100

    lab_6

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
 from . import views
-
 urlpatterns = [
+    path('persons/', views.person_list, name='person_list'),
+    path('persons/<int:pk>/', views.person_detail, name='person_detail'),
+    path('persons/update/<int:pk>/', views.person_update_delete, name='person_update_delete'),
+    path('persons/delete/<int:pk>/', views.person_update_delete, name='person_update_delete'),
     path('', views.index, name='index'),
+    path('teams/', views.team_list),
+    path('teams/<int:pk>/', views.team_detail)
 ]
